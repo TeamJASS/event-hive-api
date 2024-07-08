3caf07b4de160e762d3cba98c01c1269b8ed1e09
Adeline Ackun <adelineackun@gmail.com>
2024-07-08 14:25:44 +0000

    Setup event-hive api

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+// DB CONNECTION
+
+
+//USE EXPRESS 
+const app = express();
+
+
+
+// server set up
+app.listen (4040,() => {
+    console.log ("Live on 4040")
+})
